d755d498b9760ccace35e8128927070069fe82a7
madikarizma <madikarizma@gmail.com>
2022-07-21 01:44:11 -0400

    refactor: reorder imports

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue';
-import './style.css';
-import App from './App.vue';
 import { createRouter } from 'vue-router';
 import routerConfig from '@/router.js';
+import './style.css';
+import App from './App.vue';
 
 const router = createRouter(routerConfig);
 
